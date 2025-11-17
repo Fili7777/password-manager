5ffad986b278207bf95020ea3a0650930fddc213
Fili7777 <cucfil6@gmail.com>
2025-11-17 20:53:17 +0100

    Primo commit

---
+++ frontend/main.js
@@ -0,0 +1,36 @@
+if (document.getElementById("loginForm")) {
+  const passwordInput = document.getElementById("password");
+  const togglePassword = document.getElementById("togglePassword");
+
+  togglePassword.addEventListener("click", () => {
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text"; // mostra la password
+      togglePassword.textContent = "Nascondi";
+    } else {
+      passwordInput.type = "password"; // nasconde di nuovo
+      togglePassword.textContent = "Mostra";
+    }
+  });
+}
+
+if (document.getElementById("registerForm")) {
+  const nameInput = document.getElementById("nome");
+  const passwordInput = document.getElementById("password");
+  const togglePasswordRegister = document.getElementById(
+    "togglePasswordRegister"
+  );
+
+  togglePasswordRegister.addEventListener("click", () => {
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      togglePasswordRegister.textContent = "Nascondi";
+    } else {
+      passwordInput.type = "password";
+      togglePasswordRegister.textContent = "Mostra";
+    }
+  });
+}
+
+if (document.getElementById("dashboard")) {
+  // codice per dashboard
+}
